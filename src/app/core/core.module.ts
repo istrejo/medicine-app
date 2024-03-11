ed45557aef5d70204234f2d2e07cdbe5009c5921
istrejo <istrejo2106@gmail.com>
2024-03-11 19:57:24 -0400

    Header component in layout

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { PrimengModule } from '../primeng/primeng.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [SidebarComponent],
+  declarations: [SidebarComponent, HeaderComponent],
   imports: [CommonModule, PrimengModule, FontAwesomeModule],
-  exports: [SidebarComponent],
+  exports: [SidebarComponent, HeaderComponent],
 })
 export class CoreModule {}
