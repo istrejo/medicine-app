e7a3f1145705ab6e57507351243883818a399cda
istrejo <istrejo2106@gmail.com>
2024-03-20 00:05:46 -0400

    Doctor list

--- src/app/pages/doctor-list/doctor-list.component.ts
+++ src/app/pages/doctor-list/doctor-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Table } from 'primeng/table';
 
 interface Doctor {
   name: string;
@@ -14,71 +16,88 @@ interface Doctor {
   templateUrl: './doctor-list.component.html',
   styleUrl: './doctor-list.component.scss',
 })
-export class DoctorListComponent {
+export class DoctorListComponent implements OnInit {
+  private fb = inject(FormBuilder);
+  form!: FormGroup;
   doctors: Doctor[] = [
     {
       name: 'John Snow',
-      speciality: '2023-10-26',
-      id: '1234567890',
+      speciality: 'Traumatología',
+      id: '27919755',
       phoneNumber: '555-555-5555',
-      email: 'Main email',
+      email: 'doctor@gmail.com',
       actions: 'Schedule Appointment',
     },
     {
       name: 'John Doe',
-      speciality: '2023-10-26',
+      speciality: 'Neurología',
       id: '1234567890',
       phoneNumber: '555-555-5555',
-      email: 'Main email',
+      email: 'doctor@gmail.com',
       actions: 'Schedule Appointment',
     },
     {
       name: 'John Doe',
-      speciality: '2023-10-26',
+      speciality: 'Cirugía',
       id: '1234567890',
       phoneNumber: '555-555-5555',
-      email: 'Main email',
+      email: 'doctor@gmail.com',
       actions: 'Schedule Appointment',
     },
     {
       name: 'John Doe',
-      speciality: '2023-10-26',
+      speciality: 'Traumatología',
       id: '1234567890',
       phoneNumber: '555-555-5555',
-      email: 'Main email',
+      email: 'doctor@gmail.com',
       actions: 'Schedule Appointment',
     },
     {
       name: 'John Doe',
-      speciality: '2023-10-26',
+      speciality: 'Traumatología',
       id: '1234567890',
       phoneNumber: '555-555-5555',
-      email: 'Main email',
+      email: 'doctor@gmail.com',
       actions: 'Schedule Appointment',
     },
     {
       name: 'John Doe',
-      speciality: '2023-10-26',
+      speciality: 'Traumatología',
       id: '1234567890',
       phoneNumber: '555-555-5555',
-      email: 'Main email',
+      email: 'doctor@gmail.com',
       actions: 'Schedule Appointment',
     },
     {
       name: 'John Doe',
-      speciality: '2023-10-26',
+      speciality: 'Traumatología',
       id: '1234567890',
       phoneNumber: '555-555-5555',
-      email: 'Main email',
+      email: 'doctor@gmail.com',
       actions: 'Schedule Appointment',
     },
     {
       name: 'John Doe',
-      speciality: '2023-10-26',
+      speciality: 'Traumatología',
       id: '1234567890',
       phoneNumber: '555-555-5555',
-      email: 'Main email',
+      email: 'doctor@gmail.com',
       actions: 'Schedule Appointment',
     },
   ];
+
+  ngOnInit(): void {
+    this.initialForm();
+  }
+
+  initialForm() {
+    this.form = this.fb.group({
+      search: [''],
+    });
+  }
+
+  clear(table: Table) {
+    table.clear();
+    this.form.reset();
+  }
 }
--- src/app/pages/doctor-list/doctor-list.module.ts
+++ src/app/pages/doctor-list/doctor-list.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { DoctorListRoutingModule } from './doctor-list-routing.module';
 import { DoctorListComponent } from './doctor-list.component';
 import { PrimengModule } from '../../primeng/primeng.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [DoctorListComponent],
-  imports: [CommonModule, DoctorListRoutingModule, PrimengModule],
+  imports: [
+    CommonModule,
+    DoctorListRoutingModule,
+    PrimengModule,
+    ReactiveFormsModule,
+  ],
 })
 export class DoctorListModule {}
